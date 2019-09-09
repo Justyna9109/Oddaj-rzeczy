e2166e531b9a298eb53f7e410c8cc7f91d153694
Justyna Kasprzyk <justynakasprzyk91@wp.pl>
2019-09-09 20:20:18 +0200

    about us added

---
+++ src/components/about_us.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+class About_us extends Component{
+    render() {
+        return(<section className="about-us">
+                <div className='info'>
+                <span>O nas</span>
+                <img className='decorrat' src='/src/assets/Decoration.svg'/>
+                <p className='nori'>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.
+                Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img className='signature' src='/src/assets/Signature.svg'/>
+                </div>
+                <img className='people' src='/src/assets/People.jpg'/>
+            </section>
+
+        )
+    }
+}
+
+export default About_us;
